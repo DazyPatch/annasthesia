6d54ef88feccfda79f30acfe71a46f67c972cb01
davidcornejo <dacornejo2-c@my.cityu.edu.hk>
2026-09-17 15:20:46 +0800

    added 14th preview page

--- js/globalVariables.js
+++ js/globalVariables.js
@@ -5,6 +5,6 @@ const chapterNum = parseInt(params.get("ch"));
 
 const pagesLengths = [47, 19]
 const pagesLength = pagesLengths[chapterNum-1];
-const previewLength = 13;
+const previewLength = 14;
 
 const inVSCode = false;
